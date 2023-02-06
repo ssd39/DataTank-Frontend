949e73a8ccfa6c782684356f7620265349503ae6
ssd39 <saurabh.sachit39@gmail.com>
2023-02-06 21:32:32 +0530

    lighthouse

--- src/Pages/DetailProposal.js
+++ src/Pages/DetailProposal.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { Input, Card, Avatar, Button, Modal, Form, message } from "antd";
+import React, { useEffect, useState, useCallback } from "react";
+import { Input, Card, Avatar, Button, Modal, Form, message, Spin } from "antd";
 import "./DetailProposal.css";
 import { useLocation } from "react-router-dom";
-import abi from "../contract.js"
-const {Web3} = (window);
+import abi from "../contract.js";
+import lighthouse from "@lighthouse-web3/sdk";
+const { Web3 } = window;
 //import * as PushAPI from "@pushprotocol/restapi";
 
 const DetailProposal = () => {
@@ -17,7 +18,12 @@ const DetailProposal = () => {
   const [modalVisible, setModalVisible] = useState(false);
   const [fileCid, setFileCid] = useState("");
   const [dealId, setDealId] = useState("");
-  const [ethaddress,setEth] = useState("");
+  const [ethaddress, setEth] = useState("");
+
+  const [loading, setLoading] = useState(false);
+  const [status, setStatus] = useState("");
+
+
 
   useEffect(() => {
     (async () => {
@@ -35,8 +41,8 @@ const DetailProposal = () => {
   };
 
   const handleModal = () => {
-    setModalVisible(false)
-  }
+    setModalVisible(false);
+  };
 
   const handleTitleChange = (e) => {
     setTitle(e.target.value);
@@ -47,38 +53,38 @@ const DetailProposal = () => {
     setComment("");
   };
   const handleVote = async () => {
-    const web3 =  new Web3(window.ethereum);
+    const web3 = new Web3(window.ethereum);
     await window.ethereum.enable();
     const accounts = await web3.eth.getAccounts();
-    let contract =  new web3.eth.Contract(abi,"0x9156ecC4bA06eC3BdB696c4DCA5676D147CeB8C5")
-      
-    if(phase1Status){
+    let contract = new web3.eth.Contract(
+      abi,
+      "0x9156ecC4bA06eC3BdB696c4DCA5676D147CeB8C5"
+    );
 
-      let ans = await contract.methods.vote2(location.state.id).send({ from: accounts[0] });
-      
-    }else{
-      let ans = await contract.methods.vote(location.state.id).send({ from: accounts[0] });
-      
+    if (phase1Status) {
+      let ans = await contract.methods
+        .vote2(location.state.id)
+        .send({ from: accounts[0] });
+    } else {
+      let ans = await contract.methods
+        .vote(location.state.id)
+        .send({ from: accounts[0] });
     }
-    message.success('Proposal submitted successfully!');
-
-
-
-
-  }
+    message.success("Proposal submitted successfully!");
+  };
   useEffect(() => {
     setTitle(location.state.title);
     setDealverified(location.state.isdealverified);
     setPhase1Status(location.state.phase1Approved);
     setPhase2Status(location.state.phase2Approved);
-    async function ethsetup(){
+    async function ethsetup() {
       await window.ethereum.enable();
-    const web3 =  new Web3(window.ethereum);
-    const accounts = await web3.eth.getAccounts();
-    setEth(accounts[0]);
+      const web3 = new Web3(window.ethereum);
+      const accounts = await web3.eth.getAccounts();
+      setEth(accounts[0]);
     }
-    ethsetup()
-    console.log("the phase one status is ",location.state.phase1approved)
+    ethsetup();
+    console.log("the phase one status is ", location.state.phase1approved);
   }, [location.state]);
 
   return (
@@ -87,29 +93,67 @@ const DetailProposal = () => {
         <h2 className="text-2xl font-bold mb-4">{title}</h2>
         <div className="flex mb-4">
           <Button
-          onClick={handleVote}
+            onClick={handleVote}
             type="primary"
             className="p-2 btn-blue rounded-full flex justify-center py-2 items-center"
-            disabled = {!((!phase1Status && !location.state.votes) || (!phase2Status && !location.state.votes2 && location.state.isdealverified)) }
+            disabled={
+              !(
+                (!phase1Status && !location.state.votes) ||
+                (!phase2Status &&
+                  !location.state.votes2 &&
+                  location.state.isdealverified)
+              )
+            }
           >
             Vote
           </Button>
           <Button
             type="primary"
             className="p-2 btn-blue rounded-full ml-2 flex justify-center py-2 items-center"
-            disabled={!((ethaddress == location.state.proposer) && (phase1Status && !dealverified))}
+            disabled={
+              !(
+                ethaddress == location.state.proposer &&
+                phase1Status &&
+                !dealverified
+              )
+            }
           >
             Sample Data
           </Button>
-          {location.state.proposer == ethaddress &&
-          phase1Status ? (
+          {location.state.proposer == ethaddress && phase1Status ? (
             <>
               <Button
                 type="primary"
                 className="p-2 btn-blue rounded-full ml-2 flex justify-center py-2 items-center"
-                onClick={() => {}}
+                onClick={async () => {
+                  const input = document.createElement("input");
+                  input.type = "file";
+                  input.onchange = async (event) => {
+                    setLoading(true);
+                    setStatus("Reading file...");
+                
+                    const fileReader = new FileReader();
+                    fileReader.onload = async () => {
+                      const buffer = fileReader.result;
+                
+                      setStatus("Uploading to the lighthouse file...");
+                      // Your async function logic here
+                      //const result = await someAsyncFunction(buffer);
+                      console.log(lighthouse.uploadBuffer)
+                      const uploadResponse = await lighthouse.uploadBuffer(
+                        buffer,
+                        "ab622721-68d5-4b84-92d4-d60422299381"
+                      );
+                      console.log(uploadResponse)
+                      setStatus("File processed successfully!");
+                      setLoading(false);
+                    };
+                    fileReader.readAsArrayBuffer(event.target.files[0]);
+                  };
+                  input.click();
+                }}
               >
-                Upload with Powergate
+                Upload with lighthouse
               </Button>
               <Button
                 type="primary"
@@ -140,7 +184,7 @@ const DetailProposal = () => {
             className="my-4 flex-1 border-solid border-dotted border-2 mx-2"
           />
           <div className="flex items-center">
-            {(dealverified) ? (
+            {dealverified ? (
               <Avatar className="bg-green-500" size={24}>
                 2
               </Avatar>
@@ -154,7 +198,7 @@ const DetailProposal = () => {
             className="my-4 flex-1 border-solid border-dotted border-2 mx-2"
           />
           <div className="flex items-center">
-            {(phase2Status) ? (
+            {phase2Status ? (
               <Avatar className="bg-green-500" size={24}>
                 3
               </Avatar>
@@ -181,7 +225,9 @@ const DetailProposal = () => {
         </Card>
         {comments.map((c, i) => (
           <Card key={i} className="bg-white rounded-lg p-4 mt-4">
-            <h3 className="text-lg font-bold">Name</h3>
+            <h3 className="text-lg font-bold">
+              {window.ethereum.selectedAddress}
+            </h3>
             <p className="text-base font-medium">{c}</p>
           </Card>
         ))}
@@ -193,32 +239,46 @@ const DetailProposal = () => {
         onCancel={handleModal}
         footer={[
           <div className="flex">
-                   <Button key="submit" type="primary" onClick={handleModalSubmit}   
-          className="p-2 btn-blue rounded-full flex justify-center py-2 items-center">
-            Submit
-          </Button>
-          <Button key="back" onClick={handleModal}  type="danger"
-          className="p-2 btn-blue rounded-full flex justify-center py-2 items-center">
-            Cancel
-          </Button>
-          </div>
+            <Button
+              key="submit"
+              type="primary"
+              onClick={handleModalSubmit}
+              className="p-2 btn-blue rounded-full flex justify-center py-2 items-center"
+            >
+              Submit
+            </Button>
+            <Button
+              key="back"
+              onClick={handleModal}
+              type="danger"
+              className="p-2 btn-blue rounded-full flex justify-center py-2 items-center"
+            >
+              Cancel
+            </Button>
+          </div>,
         ]}
       >
         <Form>
           <Form.Item>
             <Input
               value={fileCid}
-              onChange={(e)=>setFileCid(e.value)}
+              onChange={(e) => setFileCid(e.value)}
               placeholder="Data CID"
             />
             <Input
               value={dealId}
               className="mt-2"
-              onChange={(e)=>setDealId(e.value)}
+              onChange={(e) => setDealId(e.value)}
               placeholder="DealId"
             />
           </Form.Item>
         </Form>
+      </Modal>
+      <Modal visible={loading}  footer={null}>
+        <div className="flex">
+        <Spin />
+        <p className="text-center ml-4">{status}</p>
+        </div>
       </Modal>
     </div>
   );
